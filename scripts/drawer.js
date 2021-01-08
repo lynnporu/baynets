ca6574ba14171fe1ec3c61d166f5b202b42cb654
Paul Lynn <lynnporu@gmail.com>
2021-01-08 10:35:51 +0200

    Add Node class

--- scripts/drawer.js
+++ scripts/drawer.js
@@ -1,23 +1,29 @@
+sandboxContainer = document.getElementById("sandbox");
 arrowsContainer = document.getElementById('arrows');
+nodesContainer = document.getElementById("nodes");
 
 class SVGElement {
 
 	element;
 
-	static createElement(name, attributes) {
-		const el = document.createElementNS(
+	constructor(name, attributes) {
+		this.element = document.createElementNS(
 			"http://www.w3.org/2000/svg", name);
+		this.updateAttributes(attributes);
+	}
+
+	updateAttributes(attributes){
 		for (const [key, value] of Object.entries(attributes))
 			el.setAttribute(key, value);
-		return el;
+
 	}
 
 }
 
-class Arrow {
+class Arrow extends SVGElement {
 
 	constructor(x1, y1, x2, y2) {
-		this.element = SVGElement.createElement("line", {
+		super("line", {
 			"x1": x1,
 			"y1": y1,
 			"x2": x2,
@@ -38,6 +44,106 @@ class Arrow {
 				"`outcome` state"
 			);
 		this.element.setAttribute("_state", name);
+	}
+
+}
+
+class Node extends SVGElement {
+
+	outcomeArrows;
+	incomeArrows;
+	_boundingRect;
+
+	refreshBound(){
+		this._boundingRect = this.element.getBoundingClientRect();
+	}
+
+	get bound(){
+		if(!this._boundingRect) this.refreshBound();
+		return this._boundingRect;
+	}
+
+	get northPoint(){
+		console.log(this.bound);
+		return [
+			this.bound.left + (this.bound.width / 2),
+			this.bound.top
+		];
+	}
+
+	get southPoint(){
+		return [
+			this.bound.left + (this.bound.width / 2),
+			this.bound.top + this.bound.height
+		];
+	}
+
+	get eastPoint(){
+		return [
+			this.bound.left + this.bound.width,
+			this.bound.top + (this.bound.height / 2)
+		];
+	}
+
+	get westPoint(){
+		return [
+			this.bound.left,
+			this.bound.top + (this.bound.height / 2)
+		];
+	}
+
+}
+
+class KnotNode extends Node {
+
+	_text;
+	rectElement;
+	textElement;
+
+	constructor(x, y, text) {
+
+		super("svg", {
+			"x": x,
+			"y": y,
+			"height": 40,
+			"class": "node-group knot"
+		});
+
+		this.text = text;
+
+		this.rectElement = SVGElement.createElement("rect", {
+			"x": 0,
+			"y": 0
+		})
+
+		this.textElement = SVGElement.createElement("text", {
+			"x": 10,
+			"y": 25
+		})
+
+		this.element.appendChild(this.rectElement);
+		this.element.appendChild(this.textElement);
+		nodesContainer.appendChild(this.element);
+
+		this.caption = text;
+		this.refreshBound();
+
+	}
+
+	set caption(text) {
+
+		this._text = text;
+		if(text.length > 90)
+			text = text.slice(0, 87) + "...";
+		this.textElement.innerHTML = text;
+
+		const rectWidth = this.textElement.getBoundingClientRect().width;
+		this.element.setAttribute("width", rectWidth + 30 + "px");
+		this.rectElement.setAttribute("width", rectWidth + 30 + "px");
+		this.textElement.setAttribute("x", (rectWidth / 2) + 15);
+
+		this.refreshBound;
+
 	}
 
 }
