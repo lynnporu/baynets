3d683af3e4b5ce4b4ba45b2523d60be9cc428092
Paul Lynn <lynnporu@gmail.com>
2021-01-09 17:20:22 +0200

    Update arrows while dragging and hovering over nodes

--- scripts/drawer.js
+++ scripts/drawer.js
@@ -2,25 +2,35 @@ sandboxContainer = document.getElementById("sandbox");
 arrowsContainer = document.getElementById('arrows');
 nodesContainer = document.getElementById("nodes");
 
+draggingNode = null;
+
 class SVGElement {
 
 	element;
 
-	constructor(name, attributes) {
-		this.element = document.createElementNS(
+	static newSVGElement(name, attributes){
+		let el = document.createElementNS(
 			"http://www.w3.org/2000/svg", name);
-		this.updateAttributes(attributes);
+		SVGElement.updateAttributes(el, attributes);
+		return el;
 	}
 
-	updateAttributes(attributes){
-		for (const [key, value] of Object.entries(attributes))
-			el.setAttribute(key, value);
+	static updateAttributes(element, attributes){
+		for (const [key, value] of Object.entries(attributes || {}))
+			element.setAttribute(key, value);
+	}
 
+	constructor(name, attributes) {
+		this.element = SVGElement.newSVGElement(name, attributes);
+		this.element.jsObj = this;
 	}
 
 }
 
 class Arrow extends SVGElement {
+
+	fromNode;
+	toNode;
 
 	constructor(x1, y1, x2, y2) {
 		super("line", {
@@ -31,6 +41,45 @@ class Arrow extends SVGElement {
 			"_state": "regular"
 		});
 		arrowsContainer.appendChild(this.element);
+	}
+
+	static betweenNodes(node1, node2) {
+
+		const arrow = new Arrow(0, 0, 0, 0);
+		arrow.connectNodes(node1, node2);
+		arrow.fromNode = node1;
+		arrow.toNode = node2;
+
+		return arrow;
+
+	}
+
+	connectNodes(node1, node2) {
+		/*This update only coordinates, not this.fromNode and this.toNode. */
+
+		const
+			possibleArrows = [
+				...combinations_no_repetition(
+					node1.connectors, node2.connectors)
+			],
+
+			shortestArrow = possibleArrows
+				.map((points) => distance(...points[0], ...points[1]))
+				.min_index(),
+
+			[point1, point2] = [...possibleArrows[shortestArrow]];
+
+		SVGElement.updateAttributes(this.element, {
+			"x1": point1[0],
+			"y1": point1[1],
+			"x2": point2[0],
+			"y2": point2[1]
+		});
+
+	}
+
+	updateNodeRelations() {
+		this.connectNodes(this.fromNode, this.toNode);
 	}
 
 	get state() {
@@ -50,21 +99,62 @@ class Arrow extends SVGElement {
 
 class Node extends SVGElement {
 
-	outcomeArrows;
-	incomeArrows;
+	outcomeArrows = [];
+	incomeArrows = [];
 	_boundingRect;
+	_clicked = false;
+	_dragging = false;
+	// moving properties
+	_orig_x = 0;
+	_orig_y = 0;
 
-	refreshBound(){
+	// Redefines SVGElement constructor
+	constructor() {
+
+		super(...arguments);
+
+		let instance = this;
+
+		this.element.addEventListener("mousedown", (e) => {
+			instance._orig_x = e.clientX;
+			instance._orig_y = e.clientY;
+			draggingNode = instance;
+		});
+
+		this.element.addEventListener("mouseover", (e) => {
+			instance.outcomeArrows.forEach((arrow) =>
+				arrow.state = "outcome");
+			instance.incomeArrows.forEach((arrow) =>
+				arrow.state = "income");
+		});
+
+		this.element.addEventListener("mouseleave", (e) => {
+			instance.outcomeArrows.forEach((arrow) =>
+				arrow.state = "regular");
+			instance.incomeArrows.forEach((arrow) =>
+				arrow.state = "regular");
+		});
+
+		this.element.addEventListener("mouseup", (e) => {
+			draggingNode = null;
+			if(!instance._dragging)
+				instance.showParametersWindow();
+			else
+				instance._dragging = false;
+		});
+
+	}
+
+	updateBounds(){
 		this._boundingRect = this.element.getBoundingClientRect();
 	}
 
 	get bound(){
-		if(!this._boundingRect) this.refreshBound();
+		if(!this._boundingRect) this.updateBounds();
 		return this._boundingRect;
 	}
 
 	get northPoint(){
-		console.log(this.bound);
 		return [
 			this.bound.left + (this.bound.width / 2),
 			this.bound.top
@@ -92,7 +182,52 @@ class Node extends SVGElement {
 		];
 	}
 
+	get connectors(){
+		return [
+			this.northPoint, this.southPoint, this.eastPoint, this.westPoint
+		];
+	}
+
+	connectTo(other){
+
+		const arrow = Arrow.betweenNodes(this, other);
+		arrow.fromNode = this;
+		arrow.toNode = other;
+		this.outcomeArrows.push(arrow);
+		other.incomeArrows.push(arrow);
+		return arrow;
+
+	}
+
+	showParametersWindow(){
+		console.log("window for", this);
+	}
+
 }
+
+document.body.addEventListener("mousemove", (e) => {
+
+	const updateArrows = (arrow) => arrow.updateNodeRelations();
+
+	if(!draggingNode) return;
+
+	draggingNode._dragging = true;
+
+	e.preventDefault();
+	// calculate the new cursor position:
+	let dx = draggingNode._orig_x - e.clientX;
+	let dy = draggingNode._orig_y - e.clientY;
+	draggingNode._orig_x = e.clientX;
+	draggingNode._orig_y = e.clientY;
+	SVGElement.updateAttributes(draggingNode.element, {
+		"x": draggingNode.element.getAttribute("x") - dx,
+		"y": draggingNode.element.getAttribute("y") - dy
+	});
+	draggingNode.updateBounds();
+	draggingNode.outcomeArrows.forEach(updateArrows);
+	draggingNode.incomeArrows.forEach(updateArrows);
+
+});
 
 class KnotNode extends Node {
 
@@ -111,12 +246,12 @@ class KnotNode extends Node {
 
 		this.text = text;
 
-		this.rectElement = SVGElement.createElement("rect", {
+		this.rectElement = SVGElement.newSVGElement("rect", {
 			"x": 0,
 			"y": 0
 		})
 
-		this.textElement = SVGElement.createElement("text", {
+		this.textElement = SVGElement.newSVGElement("text", {
 			"x": 10,
 			"y": 25
 		})
@@ -126,15 +261,15 @@ class KnotNode extends Node {
 		nodesContainer.appendChild(this.element);
 
 		this.caption = text;
-		this.refreshBound();
+		this.updateBounds();
 
 	}
 
 	set caption(text) {
 
 		this._text = text;
-		if(text.length > 90)
-			text = text.slice(0, 87) + "...";
+		if(text.length > 50)
+			text = text.slice(0, 47) + "...";
 		this.textElement.innerHTML = text;
 
 		const rectWidth = this.textElement.getBoundingClientRect().width;
@@ -142,7 +277,7 @@ class KnotNode extends Node {
 		this.rectElement.setAttribute("width", rectWidth + 30 + "px");
 		this.textElement.setAttribute("x", (rectWidth / 2) + 15);
 
-		this.refreshBound;
+		this.updateBounds;
 
 	}
 
